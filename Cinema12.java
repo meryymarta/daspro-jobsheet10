d1db530ea67e8bf9b8bfebdb001bc7465083c4f3
meryymarta <meriammartadinata@gmail.com>
2024-11-22 10:52:15 +0700

    experiment 2.1

--- Cinema12.java
+++ Cinema12.java
@@ -1,20 +1,20 @@
 public class Cinema12 {
     public static void main(String[] args) {
         
-        String[][] penonton = new String[4][2];
+        String[][] audience = new String[4][2];
 
-        penonton[0][0] = "Amin";
-        penonton[0][1] = "Bena";
-        penonton[1][0] = "Candra";
-        penonton[1][1] = "Dela";
-        penonton[2][0] = "Eka";
-        penonton[2][1] = "Farhan";
-        penonton[3][0] = "Gisel";
+        audience[0][0] = "Amin";
+        audience[0][1] = "Bena";
+        audience[1][0] = "Candra";
+        audience[1][1] = "Dela";
+        audience[2][0] = "Eka";
+        audience[2][1] = "Farhan";
+        audience[3][0] = "Gisel";
 
-        System.out.printf("%s \t %s\n", penonton[0][0], penonton[0][1]);
-        System.out.printf("%s \t %s\n", penonton[1][0], penonton[1][1]);
-        System.out.printf("%s \t %s\n", penonton[2][0], penonton[2][1]);
-        System.out.printf("%s \t %s\n", penonton[3][0], penonton[3][1]);
+        System.out.printf("%s \t %s\n", audience[0][0], audience[0][1]);
+        System.out.printf("%s \t %s\n", audience[1][0], audience[1][1]);
+        System.out.printf("%s \t %s\n", audience[2][0], audience[2][1]);
+        System.out.printf("%s \t %s\n", audience[3][0], audience[3][1]);
     }
     
 }
