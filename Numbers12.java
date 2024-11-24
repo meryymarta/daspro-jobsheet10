f4dac5f43f13d48817c0026194760569e9cc4379
meryymarta <meriammartadinata@gmail.com>
2024-11-24 13:39:48 +0700

    experiment 2.3 question 4

--- Numbers12.java
+++ Numbers12.java
@@ -8,7 +8,7 @@ public class Numbers12 {
         myNumbers[2] = new int [1]; 
 
         for(int i = 0; i < myNumbers.length; i++) {
-            System.out.println(Arrays.toString(myNumbers[i])); 
+            System.out.println("Length of row " + (i + 1) + ": " + (myNumbers[i].length)); 
         }
     }   
 }
