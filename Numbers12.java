b1c301dabacd8de0529aecc11dcca554f5de9621
meryymarta <meriammartadinata@gmail.com>
2024-11-24 13:48:31 +0700

    experiment 2.3 question 5

--- Numbers12.java
+++ Numbers12.java
@@ -2,10 +2,7 @@
 import java.util.Arrays;
 public class Numbers12 {
     public static void main(String[] args) {
-        int[][] myNumbers = new int [3][];
-        myNumbers[0] = new int [5];
-        myNumbers[1] = new int [3];
-        myNumbers[2] = new int [1]; 
+        int[][] myNumbers = new int [3][5];
 
         for(int i = 0; i < myNumbers.length; i++) {
             System.out.println("Length of row " + (i + 1) + ": " + (myNumbers[i].length)); 
