ddcf62cb1d4a313f6d12aca0b1de5efaea71ef83
meryymarta <meriammartadinata@gmail.com>
2024-11-24 08:56:58 +0700

    experiment 2.2 question 2

--- CinemaWithScanner12.java
+++ CinemaWithScanner12.java
@@ -5,10 +5,21 @@ public class CinemaWithScanner12 {
 
         int row, column;
         String name, next;
+        int menu;
 
         String[][] audience = new String[4][2];
 
-        while (true){
+        do {
+            System.out.println("CINEMA MENU");
+            System.out.println("Menu 1: Input Audience Data");
+            System.out.println("Menu 2: Show Audience Data ");
+            System.out.println("Menu 3: Exit");
+            System.out.print("Enter your menu choices: ");
+            menu = input12.nextInt();
+            input12.nextLine();
+        
+        if (menu == 1) {
+            while (true) {
             System.out.print("Enter a name: ");
             name = input12.nextLine();
             System.out.print("Enter row number: ");
@@ -16,7 +27,6 @@ public class CinemaWithScanner12 {
             System.out.print("Enter column number: ");
             column = input12.nextInt();
             input12.nextLine();
-
             audience[row - 1][column - 1] = name;
             System.out.print("Are there any other audience to be added? (y/n): ");
             next = input12.nextLine();
@@ -24,9 +34,35 @@ public class CinemaWithScanner12 {
             if (next.equalsIgnoreCase( "n")) {
                 break;
             }
+        }}
+    
+     else if (menu == 2) {
+        System.out.println("Audience list");
+        for (int i = 0; i < audience.length; i++) {
+            for(int j = 0; j < audience[i].length; j++) {
+
+        if (audience[i][j] == null) {
+            System.out.print(" ");
+
+            } else{ 
+                System.out.print(audience[i][j] + "\t");
+            }
+        }
+        System.out.println();
+    }
+    }  else if (menu == 3)  {
+        System.out.println("exiting program");
+    }    else {
+        System.out.println("Invalid choice. Please try again!");
+    }
+    } while (menu != 3);
+
         }
     }
+
+    
+    
         
-    }
+    
     
 
